d72fd243936949a7792b3ff9976396a8bb31b42e
Anton Ovechkin <owanturist@gmail.com>
2024-08-01 13:28:49 +0200

    STAC-21527 Convert glowl.js to glowl.ts

---
+++ pkg/observability/utils/growl.ts
@@ -0,0 +1,13 @@
+
+export interface HandleGrowlConfig {
+  type: string;
+  message: string;
+}
+
+export function handleGrowl(store: any, { type, message }: HandleGrowlConfig) {
+  store.dispatch(`growl/${ type }`, {
+    title:   'Error',
+    message,
+    timeout: 5000,
+  }, { root: true });
+}
