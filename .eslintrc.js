fdef60afcaa296f880c8d561d494121cff3d6f2a
Anton Ovechkin <owanturist@gmail.com>
2024-10-23 16:59:48 +0200

    Do not extend cypress and install missing deps for standart

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "plugin:vue/vue3-strongly-recommended",
     "plugin:vue/vue3-recommended",
-    "plugin:cypress/recommended"
   ],
   "rules": {
     "dot-notation": "off",
